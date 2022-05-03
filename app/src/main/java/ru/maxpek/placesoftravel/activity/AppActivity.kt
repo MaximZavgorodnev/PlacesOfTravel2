5e39ab07e5e3cb647dcb19c17cf8a949a9dfacce
MaximZavgorodnev <maxpek9@yandex.ru>
2022-05-03 16:33:10 +0300

    Ничего не работает все также

--- app/src/main/java/ru/maxpek/placesoftravel/activity/AppActivity.kt
+++ app/src/main/java/ru/maxpek/placesoftravel/activity/AppActivity.kt
@@ -1,14 +1,45 @@
 package ru.maxpek.placesoftravel.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.mapview.MapView
 import ru.maxpek.placesoftravel.R
 import ru.maxpek.placesoftravel.databinding.AppActivityBinding
 
+
 class AppActivity : AppCompatActivity(){
+    companion object{
+        private const val MAPKIT_API_KEY = "e0f40ead-fefb-45cf-821c-37efc0eaa548"
+        private lateinit var mapView: MapView
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = AppActivityBinding.inflate(layoutInflater)
+//        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+//        MapKitFactory.initialize(this)
+//
+//        setContentView(R.layout.fragment_maps)
+//        mapView = findViewById<View>(R.id.map) as MapView
+//        mapview.getMap().move(
+//            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+//            Animation(Animation.Type.SMOOTH, 0),
+//            null
+//        )
         setContentView(binding.root)
+//        val mapKit = MapKitFactory.getInstance()
+    }
+
+        override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MapKitFactory.getInstance().onStop()
+        mapView.onStop()
     }
 }
--- app/src/main/java/ru/maxpek/placesoftravel/activity/MapsFragment.kt
+++ app/src/main/java/ru/maxpek/placesoftravel/activity/MapsFragment.kt
@@ -1,109 +1,31 @@
 package ru.maxpek.placesoftravel.activity
 
+import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil.setContentView
-import androidx.fragment.app.Fragment
-import com.yandex.mapkit.MapKit
 import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.MapKitFactory.setApiKey
 import com.yandex.mapkit.mapview.MapView
 import ru.maxpek.placesoftravel.R
-import ru.maxpek.placesoftravel.R.*
+
 
 class MapsFragment : Fragment() {
-    private lateinit var mapView: MapView
-
-
-//
-//    @SuppressLint("MissingPermission")
-//    private val requestPermissionLauncher =
-//        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-//            if (isGranted) {
-//                yandexMap.apply {
-//                    this.createUserLocationLayer(mapView.findViewById(R.id.map))
-//                }
-//            } else {
-//                // TODO: show sorry dialog
-//            }
-//        }
 
     companion object{
         private const val MAPKIT_API_KEY = "e0f40ead-fefb-45cf-821c-37efc0eaa548"
+        private lateinit var mapView: MapView
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        savedInstanceState.apply {
-//            val yandexMap = MapKitFactory.getInstance()
-            mapView.apply {
-                setApiKey(MAPKIT_API_KEY)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        MapKitFactory.setApiKey(MAPKIT_API_KEY)
+        MapKitFactory.initialize(context)
+        mapView.findViewById<View>(R.id.map) as MapView
 
-                addView(view)
-            }
-        }
+        return inflater.inflate(R.layout.fragment_maps, container)
     }
-
-
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//
-//
-//        val view : View = inflater.inflate(layout.fragment_maps)
-////        val binding = FragmentMapsBinding.inflate(inflater)
-//        val yandexMap = MapKitFactory.getInstance()
-////        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-////            View mainView = inflater.inflate(R.layout.yandex_map, container);
-////            return mainView;
-////
-////            View mainView = inflater.inflate(R.layout.yandex_map, null)
-//
-//
-////        MapKitFactory.initialize(context)
-////        var mapViewBundle: Bundle? = null
-////        if (savedInstanceState != null) {
-////            mapViewBundle = savedInstanceState.getBundle(MAPKIT_API_KEY)
-////        }
-
-//        return view
-//    }
-
-
-//    override fun onStart() {
-//        super.onStart()
-//        MapKitFactory.getInstance().onStart()
-//        mapView.onStart()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        MapKitFactory.getInstance().onStop()
-//        mapView.onStop()
-//    }
 }
-
-
-//
-//public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-//    View mainView = inflater.inflate(layout.yandex_map, container);
-//    return mainView;
-//}
-//
-//
-//
-//
-//yandex_map.xml выглядит вот так:
-//
-//
-//<ru.yandex.yandexmapkit.MapView
-//android:id="@+id/map"
-//android:layout_width="fill_parent"
-//android:layout_height="fill_parent"
-//android:apiKey="мой ключ"
-//android:layout_weight="1"
-///>
